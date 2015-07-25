b221494d24ff725d4314403e5e74797ed047aaf5
Gaurav Kumar <gaurav.kumar@chronus.com>
2015-07-25 05:38:16 +0530

    indentation done

--- app/controllers/github_api_controller.rb
+++ app/controllers/github_api_controller.rb
@@ -3,32 +3,32 @@ class GithubApiController < ApplicationController
   def index
   end
 
-	def show
-	end
+  def show
+  end
 
-	def list
-		@user = params[:user]
-		@repo = params[:repo]
-		repos = Github::Client::Repos.new
-	  @response = repos.branches @user, @repo
-	  @commits = repos.commits.list @user, @repo
-	  @releases = repos.releases.list @user, @repo
+  def list
+    @user = params[:user]
+    @repo = params[:repo]
+    repos = Github::Client::Repos.new
+    @response = repos.branches @user, @repo
+    @commits = repos.commits.list @user, @repo
+    @releases = repos.releases.list @user, @repo
 
-	end
+  end
 
-	def create
-		@user = params[:user]
-		password = params[:password]
+  def create
+    @user = params[:user]
+    password = params[:password]
     @github = Github.new basic_auth: "#{@user}:#{password}"
 
     begin
-    	@github.repos.list
+      @github.repos.list
     rescue
-	    flash[:error] = "Authentication Error!!"
-	  	@error = true
-	  end
+      flash[:error] = "Authentication Error!!"
+      @error = true
+    end
     
     render "show"
-	end
+  end
 
 end
